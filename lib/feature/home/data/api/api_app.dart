44df43ca2c91703dacf304cc59fb77ce7ba2187f
shahd_ahmed <shahdahmed8883@gmail.com>
2025-11-23 19:48:40 +0200

    add api

--- lib/feature/home/data/api/api_app.dart
+++ lib/feature/home/data/api/api_app.dart
@@ -3,7 +3,6 @@ import 'package:http/http.dart' as http;
 
 class HomeApi{
   static getDataHome()async{
-
     //https://newsapi.org/v2/everything?q=bitcoin&apiKey=85de9ad38c00463ab7471fcfc793627d
     Uri url = Uri.https('newsapi.org','/v2/everything',{
       'apiKey': '85de9ad38c00463ab7471fcfc793627d',
